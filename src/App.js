d1f41655702568ef1683fa3203afdc399c846faa
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-04-09 23:29:08 +0530

    delete bus Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Add from './components/Add';
+import Delete from './components/Delete';
 
 function App() {
   return (
    <div>
 <Add/>
-
+<Delete/>
 
 
 
---
+++ src/components/Delete.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Delete = () => {
+  return (
+    <div>
+<div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label"><b>BUS ID</b></label>
+                                <input type="number" className="form-control" />
+                            </div>
+                           
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                    <button className="btn btn-danger"><b>DELETE</b></button>
+                                </div>
+
+                       </div>
+                       </div>
+                       </div>
+                       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Delete
