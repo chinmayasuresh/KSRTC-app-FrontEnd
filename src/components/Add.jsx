e39d8edf7f17dd24ddc640b9174a7d3371319086
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-04-09 23:24:23 +0530

    add bus Completed

---
+++ src/components/Add.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+<div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label"><b>BUS ID</b></label>
+                                <input type="number" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label"><b>NUMBER</b></label>
+                                <input type="number" name="" id="" className="form-control" />
+                            </div>
+
+                           
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label"><b>ROUTE</b></label>
+                                <input type="text" name="" id="" className="form-control" />
+                            </div>
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label"><b>DRIVER NAME</b></label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label"><b>DRIVER NUMBER</b></label>
+                                <input type="text" className="form-control" />
+                            </div>
+
+
+                     
+                                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                    <button className="btn btn-success"><b>SUBMIT</b></button>
+                                </div>
+                            </div>
+                
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Add
