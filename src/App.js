79d7529a176ba9421a4c2a8b8af5042a8abbdfbb
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-04-09 23:48:48 +0530

    search bus Completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Add from './components/Add';
 import Delete from './components/Delete';
 import Search from './components/Search';
 import View from './components/View';
+import Nav from './components/Nav';
 
 function App() {
   return (
-   <div>
-<Add/>
-<Delete/>
+    <div>
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Main/>}   />
+          <Route path="/add" element={<Add />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/delete" element={<Delete />} />
+          <Route path="/view" element={<View />} />
+        </Routes>
 
-<Search/>
-
-<View/>
-   </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
